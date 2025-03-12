2ce91fd5e8bcb53bdc6050cdaa4377453e3c6381
rameshb1411 <rameshb1411@gmail.com>
2025-03-12 20:42:44 +0530

    Upload files from local directory

---
+++ device_config.py
@@ -0,0 +1,9 @@
+DEVICE_CONFIG = {
+    "simulation_mode": True,
+    "modem": {
+        "ip": "192.168.1.1",
+        "port": 22,
+        "username": "admin",
+        "password": "password"
+    }
+}
