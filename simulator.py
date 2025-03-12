914bd29f1d1984dd45a66169bf76cc2495b25983
rameshb1411 <rameshb1411@gmail.com>
2025-03-12 20:42:39 +0530

    Upload files from local directory

---
+++ simulator.py
@@ -0,0 +1,161 @@
+import random
+
+class RANSimulator:
+    def __init__(self):
+        self.frequency_bands = ["700MHz", "800MHz", "900MHz", "1800MHz", "2100MHz"]
+        self.signal_strength = {
+            "700MHz": -70,
+            "800MHz": -65,
+            "900MHz": -60,
+            "1800MHz": -55,
+            "2100MHz": -50
+        }
+        self.modulation_schemes = ["QPSK", "16QAM", "64QAM", "256QAM"]
+        self.carrier_aggregation_supported = True
+        self.beamforming_supported = True
+        self.mimo_supported = True
+        self.nr_carrier_aggregation_supported = True
+        self.lte_bandwidth = 20  # MHz
+        self.lte_ul_dl_ratio = 4
+        self.nr_bandwidth = 100  # MHz
+        self.nr_ul_dl_ratio = 2
+        self.nr_massive_mimo_supported = True
+        self.lte_harq_supported = True
+        self.lte_power_control_supported = True
+        self.nr_harq_supported = True
+        self.nr_power_control_supported = True
+        self.network_slicing_supported = True
+
+    def get_supported_bands(self):
+        return self.frequency_bands
+
+    def measure_signal_strength(self, band):
+        return self.signal_strength.get(band, "Unknown band")
+
+    def get_modulation_schemes(self):
+        return self.modulation_schemes
+
+    def measure_throughput(self):
+        return random.uniform(50.0, 100.0)  # Mbps
+
+    def measure_latency(self):
+        return random.uniform(10.0, 50.0)  # ms
+
+    def handover_success_rate(self):
+        return random.uniform(95.0, 100.0)  # Percentage
+
+    def measure_signal_quality(self):
+        snr = random.uniform(20.0, 30.0)  # dB
+        ber = random.uniform(0.0001, 0.001)  # Bit Error Rate
+        return {"SNR": snr, "BER": ber}
+
+    def check_carrier_aggregation_support(self):
+        return self.carrier_aggregation_supported
+
+    def check_beamforming_support(self):
+        return self.beamforming_supported
+
+    def check_mimo_support(self):
+        return self.mimo_supported
+
+    def check_nr_carrier_aggregation_support(self):
+        return self.nr_carrier_aggregation_supported
+
+    def get_lte_bandwidth(self):
+        return self.lte_bandwidth
+
+    def get_lte_ul_dl_ratio(self):
+        return self.lte_ul_dl_ratio
+
+    def get_nr_bandwidth(self):
+        return self.nr_bandwidth
+
+    def get_nr_ul_dl_ratio(self):
+        return self.nr_ul_dl_ratio
+
+    def check_nr_massive_mimo_support(self):
+        return self.nr_massive_mimo_supported
+
+    def check_lte_harq_support(self):
+        return self.lte_harq_supported
+
+    def check_lte_power_control_support(self):
+        return self.lte_power_control_supported
+
+    def check_nr_harq_support(self):
+        return self.nr_harq_supported
+
+    def check_nr_power_control_support(self):
+        return self.nr_power_control_supported
+
+    def check_nr_network_slicing_support(self):
+        return self.network_slicing_supported
+
+
+class ModemSimulator:
+    def __init__(self, ran_simulator):
+        self.ran_simulator = ran_simulator
+
+    def check_band_support(self, band):
+        supported_bands = self.ran_simulator.get_supported_bands()
+        return band in supported_bands
+
+    def get_signal_strength(self, band):
+        return self.ran_simulator.measure_signal_strength(band)
+
+    def get_modulation_schemes(self):
+        return self.ran_simulator.get_modulation_schemes()
+
+    def get_throughput(self):
+        return self.ran_simulator.measure_throughput()
+
+    def get_latency(self):
+        return self.ran_simulator.measure_latency()
+
+    def get_handover_success_rate(self):
+        return self.ran_simulator.handover_success_rate()
+
+    def get_signal_quality(self):
+        return self.ran_simulator.measure_signal_quality()
+
+    def check_carrier_aggregation_support(self):
+        return self.ran_simulator.check_carrier_aggregation_support()
+
+    def check_beamforming_support(self):
+        return self.ran_simulator.check_beamforming_support()
+
+    def check_mimo_support(self):
+        return self.ran_simulator.check_mimo_support()
+
+    def check_nr_carrier_aggregation_support(self):
+        return self.ran_simulator.check_nr_carrier_aggregation_support()
+
+    def get_lte_bandwidth(self):
+        return self.ran_simulator.get_lte_bandwidth()
+
+    def get_lte_ul_dl_ratio(self):
+        return self.ran_simulator.get_lte_ul_dl_ratio()
+
+    def get_nr_bandwidth(self):
+        return self.ran_simulator.get_nr_bandwidth()
+
+    def get_nr_ul_dl_ratio(self):
+        return self.ran_simulator.get_nr_ul_dl_ratio()
+
+    def check_nr_massive_mimo_support(self):
+        return self.ran_simulator.check_nr_massive_mimo_support()
+
+    def check_lte_harq_support(self):
+        return self.ran_simulator.check_lte_harq_support()
+
+    def check_lte_power_control_support(self):
+        return self.ran_simulator.check_lte_power_control_support()
+
+    def check_nr_harq_support(self):
+        return self.ran_simulator.check_nr_harq_support()
+
+    def check_nr_power_control_support(self):
+        return self.ran_simulator.check_nr_power_control_support()
+
+    def check_nr_network_slicing_support(self):
+        return self.ran_simulator.check_nr_network_slicing_support()
